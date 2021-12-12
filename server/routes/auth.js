78d3bda3051516330afff7379d13b7ae48706663
Deepanshu goyal <500052753@stu.upes.ac.in>
2021-12-12 22:32:54 +0530

    Added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express= require("express");
 const router= express.Router();
-const {signUp, signIn}= require("../controllers/auth");
+const {signUp, signIn,getUsers,follow}= require("../controllers/auth");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
-router.get("/users", user);
+router.get("/users", getUsers);
+router.post("/follow", follow);
 
 module.exports= router;
