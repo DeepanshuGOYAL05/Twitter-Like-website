a854f09516cecb15ed1f2c1fa3b21c6d10c22704
Deepanshu goyal <500052753@stu.upes.ac.in>
2021-12-12 22:32:54 +0530

    Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,8 +68,8 @@ exports.getUsers=async function(req,res,next)
 {
   try {
     let users=await db.User.find({ 'username': new RegExp(req.searchTerm, 'i'),
-    'email':new RegExp(req.searchTerm,'i') 
-     }, process.env.SECRET_KEY);
+    'email':new RegExp(req.searchTerm,'i') }
+   , {email:1,username:1,password:1 });
   
     return res.status(200).json({
         users })
@@ -83,4 +83,38 @@ catch(err){
             message: err.message
         })
     }
+};
+
+exports.follow=async function(req,res,next)
+{
+    try {
+        let followingUser=await db.User.find({email:req.followingEmail || "goyaldeepanshu0098@gmail.com"});
+        let followedUser=await db.User.find({email:req.followedEmail || "arpit.bhardwaj@gmail.com"});
+        console.log(JSON.stringify(followingUser));
+        console.log(JSON.stringify(followedUser));
+             
+         followingUser.following.push({
+          email: req.followedEmail 
+        })
+       
+        followedUser.followers.push({
+         email: req.followingEmail});
+
+        await db.User.update({email:req.followingEmail || "goyaldeepanshu0098@gmail.com"},{$set:followingUser});
+        await db.User.update({email:req.followedEmail || "arpit.bhardwaj@gmail.com"},{$set:followedUser});
+        return res.status(200).json(
+            { "message" :"User follwed succesfully" }
+     )
+    }
+    catch(err){
+        if (err.code === 11000) {
+                err.message = "Sorry that username or email is already taken";
+            }
+            return next({
+                status: 400,
+                message: err.message
+            })
+
+        }
+
 };
