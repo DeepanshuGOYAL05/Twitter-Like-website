5e0859418c2ed0721b296a6cde737698c9ad9271
Deepanshu goyal <500052753@stu.upes.ac.in>
2021-12-20 00:12:57 +0530

    Code

--- server/index.js
+++ server/index.js
@@ -11,11 +11,15 @@ const errorHandler= require("./controllers/error");
 
 //serve static assets in production
 if (process.env.NODE_ENV === "production") {
-    //set static folder
+    //set static folder\
+    console.log("jsdj");
     app.use(express.static("./client/build"));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,"./", "client", "build", "index.html"));
+        console.log("***************");
+        console.log(path.resolve(__dirname,".", "client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname,".", "client", "build", "index.html"));
+      
     });
   }
 
