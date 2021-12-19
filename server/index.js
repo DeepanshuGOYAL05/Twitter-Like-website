372a7e2cf489b74db7bb8c48d9339ca951a1d32e
Deepanshu goyal <500052753@stu.upes.ac.in>
2021-12-19 23:06:28 +0530

    Code dded

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ const express= require("express"),
 
 const errorHandler= require("./controllers/error");
 
+
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+
 // Routes
 const routes= require("./routes");
 
