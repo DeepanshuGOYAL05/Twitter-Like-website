2bc6be85efa784cf98ea972a7d34690f58959cd4
Deepanshu goyal <500052753@stu.upes.ac.in>
2021-12-19 22:39:54 +0530

    Code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
 const express= require("express");
 const router= express.Router();
-const {signUp, signIn,getUsers,follow,userData}= require("../controllers/auth");
+const {signUp, signIn,getUsers,follow}= require("../controllers/auth");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.get("/users", getUsers);
 router.post("/follow", follow);
-router.get("userData",userData);
+
 
 module.exports= router;
